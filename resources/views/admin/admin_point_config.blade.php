73b6942a65f587fce5d1e5f19db805933d05d5f3
kanno shunsuke <syunsuke.05.23.15@gmail.com>
2026-09-12 12:33:54 +0900

    ポイント管理画面調整

--- resources/views/admin/admin_point_config.blade.php
+++ resources/views/admin/admin_point_config.blade.php
@@ -10,56 +10,69 @@
             </div>
         @endif
 
-        <div class="table-responsive">
-            <table class="table table-hover align-middle">
-                <thead class="table-light">
-                    <tr class="row g-0">
-                        <th class="col-4">設定名</th>
-                        <th class="col-3">ﾊﾟﾗﾒｰﾀ</th>
-                        <th class="col-1">タイプ</th>
-                        <th class="col-1">価格</th>
-                        <th class="col-1">付与pt</th>
-                        <th class="col-1"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($configs as $config)
-                        <tr class="row g-0">
-                            <td class="col-4 small text-muted">{{ $config->description }}</td>
-                            <td class="col-3"><code>{{ $config->config_name }}</code></td>
-                            <td class="col-1 small text-muted">{{ $config->type }}</td>
+        <div class="container-fluid p-0">
+    <!-- ヘッダー行 -->
+    <div class="row g-2 fw-bold bg-light py-2 px-1 border-bottom align-items-center text-muted small">
+        <div class="col-md-3">設定名</div>
+        <div class="col-md-2">パラメータ</div>
+        <div class="col-md-1">タイプ</div>
+        <div class="col-md-4">設定値</div>
+        <div class="col-md-2"></div>
+    </div>
 
-                            <td class="col-3 p-0">
-                                <form action="{{ route('admin.point.config.update') }}" method="POST" class="row g-0 h-100">
-                                    @csrf
-                                    <input type="hidden" name="config_name" value="{{ $config->config_name }}">
+    <!-- データ行 -->
+    @foreach($configs as $config)
+        <div class="row g-2 align-items-center border-bottom py-2 m-0">
+            <form action="{{ route('admin.point.config.update') }}" method="POST" class="row g-2 align-items-center w-100 m-0 p-0">
+                @csrf
+                <!-- 識別用・送信用の hidden フィールドを追加 -->
+                <input type="hidden" name="config_name" value="{{ $config->config_name }}">
+                <input type="hidden" name="type" value="{{ $config->type }}">
 
-                                    @if($config->type == 'int' || $config->type == 'range')
-                                        <div class="col-4 p-1"><input type="number" inputmode="numeric" min="-99999" max="99999" name="value1" value="{{ $config->value1 }}" class="form-control form-control-sm"></div>
-                                        <div class="col-4 p-1"><input type="number" inputmode="numeric" min="-99999" max="99999" name="value2" value="{{ $config->value2 }}" class="form-control form-control-sm"></div>
-                                    @elseif($config->type == 'string')
-                                        <div class="col-4 p-1"><input type="text" name="value1" value="{{ $config->value1 }}" class="form-control form-control-sm"></div>
-                                        <div class="col-4 p-1"><input type="text" name="value2" value="{{ $config->value2 }}" class="form-control form-control-sm"></div>
-                                    @elseif($config->type == 'bool')
-                                        <div class="col-4 p-1">
-                                            <input type="hidden" name="value1" value="0">
-                                            <div class="form-check">
-                                                <input class="form-check-input" type="checkbox" id="value1_{{ $config->config_name }}" name="value1" value="1" {{ $config->value1 ? 'checked' : '' }}>
-                                                <label class="form-check-label" for="value1_{{ $config->config_name }}">有効</label>
-                                            </div>
-                                        </div>
-                                        <div class="col-4 p-1"></div>
-                                    @endif
+                <div class="col-md-3">
+                    <input type="text" name="description" value="{{ $config->description }}" class="form-control form-control-sm">
+                </div>
+                <div class="col-md-2">
+                    <code>{{ $config->config_name }}</code>
+                </div>
+                <div class="col-md-1 small text-muted">
+                    {{ $config->type }}
+                </div>
 
-                                    <div class="col-4 p-1">
-                                        <button type="submit" class="btn btn-primary btn-sm w-100">保存</button>
-                                    </div>
-                                </form>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                <div class="col-md-4">
+                    <div class="row g-1">
+                        @if($config->type == 'int' || $config->type == 'range')
+                            <div class="col-6">
+                                <input type="number" inputmode="numeric" min="-99999" max="99999" name="value1" value="{{ $config->value1 }}" class="form-control form-control-sm">
+                            </div>
+                            <div class="col-6">
+                                <input type="number" inputmode="numeric" min="-99999" max="99999" name="value2" value="{{ $config->value2 }}" class="form-control form-control-sm">
+                            </div>
+                        @elseif($config->type == 'string')
+                            <div class="col-6">
+                                <input type="text" name="value1" value="{{ $config->value1 }}" class="form-control form-control-sm">
+                            </div>
+                            <div class="col-6">
+                                <input type="text" name="value2" value="{{ $config->value2 }}" class="form-control form-control-sm">
+                            </div>
+                        @elseif($config->type == 'bool')
+                            <div class="col-12">
+                                <input type="hidden" name="value1" value="0">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" id="value1_{{ $config->config_name }}" name="value1" value="1" {{ $config->value1 ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="value1_{{ $config->config_name }}">有効</label>
+                                </div>
+                            </div>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="col-md-2">
+                    <button type="submit" class="btn btn-primary btn-sm w-100">保存</button>
+                </div>
+            </form>
         </div>
+    @endforeach
+</div>
     </div>
 </div>
